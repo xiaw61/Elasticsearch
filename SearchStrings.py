8b4f84f110dd634b8bf18a15da022abb8c2a999e
xiaw61 <xiaw61@gmail.com>
2014-10-27 15:36:40 -0400

    Fixed the following bugs:
    1. All docs need to be indexed each search.
    2. ES does not close properly.

--- SearchStrings.py
+++ SearchStrings.py
@@ -17,34 +17,30 @@ class SearchStrings:
     def __init__(self):
         self.es = Elasticsearch()
 
-    # Input the file name that need to be indexed
-    def save_text2index(self, file_name):
-        try:
-            fo = codecs.open(file_name, encoding='utf-8', errors='strict')
-            content = {"fileName": fo.name, "text": fo.read()}
-            fo.close()
-            self.es.index(index="test-index", doc_type='tweet', body=content)
-            self.es.indices.refresh(index="test-index")
-        # If the file if not valid "utf-8" file, do not index it
-        except UnicodeDecodeError:
-            pass
+    def save_text2engine(self):
+        for root, subFolders, files in os.walk(TARGET_FOLDER):
+            for dataFile in files:
+                file_name = root + "/" + dataFile
+                try:
+                    print file_name
+                    fo = codecs.open(file_name, encoding='utf-8', errors='strict')
+                    content = {"fileName": fo.name, "text": fo.read()}
+                    fo.close()
+                    self.es.index(index="test-index", doc_type='tweet', body=content, id=file_name)
+                    self.es.indices.refresh(index="test-index")
+                # If the file if not valid "utf-8" file, do not index it
+                except UnicodeDecodeError:
+                    pass
 
     # The method that search all files in the target directory than contain the query string list
     def search_func(self, query_string):
         result_dict = {}
-        for root, subFolders, files in os.walk(TARGET_FOLDER):
-            for dataFile in files:
-                file_name = root + "/" + dataFile
-                self.save_text2index(file_name)
-            if len(files) > 0:
-                res = self.es.search(index="test-index",
-                                     body={"query": {"match": {"text": {"query": query_string, "operator": "and"}}}},
-                                     size=10)
-                self.es.indices.delete(index="test-index")
-                for hit in res['hits']['hits']:
-                    result_dict[hit["_source"]["fileName"]] = hit["_source"]["text"]
-                    if len(result_dict) == 10:
-                        return result_dict
+        res = self.es.search(index="test-index",
+                             body={"query": {"match": {"text": {"query": query_string, "operator": "and"}}}},
+                             size=10)
+        # self.es.indices.delete(index="test-index")
+        for hit in res['hits']['hits']:
+            result_dict[hit["_source"]["fileName"]] = hit["_source"]["text"]
         return result_dict
 
     # convert the direction to string in print format
@@ -68,19 +64,24 @@ def main():
 
     search = SearchStrings()
     query_strings = " ".join(sys.argv[1:])
+
     result_list = None
     p = None
     try:
         print "Launch the search engine..."
         p = subprocess.Popen([ELASTICSEARCH_LAUNCH_CMD], stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
         search.is_elasticsearch_ready()
+        print "Indexing the target files to search engine"
+        search.save_text2engine();
+
         print "Start search the query strings in the target folder"
         result_list = search.search_func(query_strings)
+
         print 'The following files contain " ' + query_strings + ' ":'
         SearchStrings.print_helper(result_list)
     finally:
         # Always close the search engine
-        p.kill()
+        search.es.nodes.shutdown(node_id="_all")
 
 
 if __name__ == '__main__':
